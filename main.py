df1e11a8b1a8d1f010971bdccd9b60ea5089a494
soraxem <admin@linuxtechlab.com>
2020-04-05 02:44:08 +0200

    added ImgTasks

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ myobj = {'': 'Der Hannes'}
 x = requests.post(url, data = myobj)
 print(x.text)
 
-def loadtextTask():
+def loadTextTask():
     global tree
     global root
     text = ""
@@ -32,17 +32,70 @@ def loadtextTask():
     text = text[:-5:] #delete last Linebreak
     #Text Completed
 
-    table = "" #table init
-    i = 0
-    for options in root.findall("options"):
-        table = table + "<tr>"
-        for option in options.findall("option"):
-            table = table + '<td onclick="eel.answer(' + str(i) +',1)">' + option.text + "</td>"
-            i += 1
+    print(root.attrib["reply-type"])
+    if (root.attrib["reply-type"] == "options"):
+        print("Teel")
+        table = "" #table init
+        i = 0
+        for options in root.findall("options"):
+            table = table + "<tr>"
+            for option in options.findall("option"):
+                table = table + '<td onclick="eel.answer(' + str(i) +',1)">' + option.text + "</td>"
+                i += 1
 
-        table = table + "</tr>"
-    #table completed
-    eel.loadtextTask(text, table)
+            table = table + "</tr>"
+        #table completed
+        eel.loadTextTaskOptions(text, table)
+
+    elif (root.attrib["reply-type"] == "input"):
+        if (root.attrib["input-type"] == "number"):
+            eel.loadTextTaskNumber(text)
+        elif (root.attrib["input-type"] == "text"):
+            eel.loadTextTaskText(text)
+        else:
+            print("Error! ")
+    else:
+        print("Error! invlid XML Statement")
+
+
+def loadImgTask():
+    global tree
+    global root
+    text = ""
+    for t in root.findall("text"): #add linebreaks
+        text = text + t.text + "</br>"
+    text = text[:-5:] #delete last Linebreak
+    #Text Completed
+
+    url = ""
+    for t in root.findall("img"):
+        url = t.text
+
+    print(root.attrib["reply-type"])
+    if (root.attrib["reply-type"] == "options"):
+        print("Teel")
+        table = "" #table init
+        i = 0
+        for options in root.findall("options"):
+            table = table + "<tr>"
+            for option in options.findall("option"):
+                table = table + '<td onclick="eel.answer(' + str(i) +',1)">' + option.text + "</td>"
+                i += 1
+
+            table = table + "</tr>"
+        #table completed
+        eel.loadImgTaskOptions(text, table, url)
+
+    elif (root.attrib["reply-type"] == "input"):
+        if (root.attrib["input-type"] == "number"):
+            eel.loadImgTaskNumber(text, url)
+        elif (root.attrib["input-type"] == "text"):
+            eel.loadImgTaskText(text, url)
+        else:
+            print("Error! ")
+    else:
+        print("Error! invlid XML Statement")
+    
 
 @eel.expose
 def answer(value, form):
@@ -53,13 +106,15 @@ def answer(value, form):
 def load():
     global tree
     global root
-    tree = ET.parse('Task1.xml')
+    tree = ET.parse('Task5.xml')
     root = tree.getroot()
     if (root.tag != "task"):
         print("Error! False root argument -> it Has to be 'task' not: '" + root.tag + "'")
     else:
         if (root.attrib["type"] == "text"):
-            loadtextTask()
+            loadTextTask()
+        elif (root.attrib["type"] == "img"):
+            loadImgTask()
 
 
 
