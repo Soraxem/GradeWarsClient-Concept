39c2881149df7de4b6c69f9a07301e54d6685210
soraxem <admin@linuxtechlab.com>
2020-04-05 02:42:02 +0200

    added imgTasks

--- web/main.js
+++ web/main.js
@@ -1,20 +1,102 @@
 alert("test")
 
-eel.expose(loadtextTask);
-function loadtextTask(text, table){
+eel.expose(loadTextTaskOptions);
+function loadTextTaskOptions(text, table){
 	document.getElementById("textTask").style.opacity = 1;
+	document.getElementById("textTask").style.zIndex= 1;
 	document.getElementById("imgTask").style.opacity = 0;
-	document.getElementById("taskText").innerHTML = text;
+	document.getElementById("imgTask").style.zIndex= -1;
+	
+	document.getElementById("textTaskOptions").style.display = "table";
+	document.getElementById("textTaskNumber").style.display = "none";
+	document.getElementById("textTaskText").style.display = "none";
+	
+	document.getElementById("taskTextText").innerHTML = text;
 	document.getElementById("textTaskOptions").innerHTML = table;
 }
 
-eel.expose(loadimgTask);
-function loadimgTask(text, table){
-	document.getElementById("textTask").style.opacity = 0;
-	document.getElementById("imgTask").style.opacity = 1;
-	document.getElementById("taskText").innerHTML = text;
-	document.getElementById("taskTable2").innerHTML = table;
+eel.expose(loadTextTaskNumber);
+function loadTextTaskNumber(text){
+	
+	document.getElementById("textTaskOptions").style.display = "none";
+	document.getElementById("textTaskNumber").style.display = "inline";
+	document.getElementById("textTaskText").style.display = "none";
+	
+	document.getElementById("textTask").style.opacity = 1;
+	document.getElementById("textTask").style.zIndex= 1;
+	document.getElementById("imgTask").style.opacity = 0;
+	document.getElementById("imgTask").style.zIndex= -1;
+	
+	document.getElementById("taskTextText").innerHTML = text;
 }
+
+eel.expose(loadTexttextTaskText);
+function loadTexttextTaskText(text){
+	
+	document.getElementById("textTaskOptions").style.display = "none";
+	document.getElementById("textTaskNumber").style.display = "none";
+	document.getElementById("textTaskText").style.display = "inline";
+	
+	document.getElementById("textTask").style.opacity = 1;
+	document.getElementById("textTask").style.zIndex= 1;
+	document.getElementById("imgTask").style.opacity = 0;
+	document.getElementById("imgTask").style.zIndex= -1;
+	
+	document.getElementById("taskTextText").innerHTML = text;
+}
+
+
+
+eel.expose(loadImgTaskOptions);
+function loadImgTaskOptions(text, table, url){
+	document.getElementById("textTask").style.opacity = 0;
+	document.getElementById("textTask").style.zIndex= -1;
+	document.getElementById("imgTask").style.opacity = 1;
+	document.getElementById("imgTask").style.zIndex= 1;
+	
+	document.getElementById("imgTaskOptions").style.display = "table";
+	document.getElementById("imgTaskNumber").style.display = "none";
+	document.getElementById("imgTaskText").style.display = "none";
+	
+	document.getElementById("taskTextImg").innerHTML = text;
+	document.getElementById("imgTaskOptions").innerHTML = table;
+	
+	document.getElementById("imgTaskImg").src = url;
+}
+
+eel.expose(loadImgTaskNumber);
+function loadImgTaskNumber(text, url){
+	document.getElementById("textTask").style.opacity = 0;
+	document.getElementById("textTask").style.zIndex= -1;
+	document.getElementById("imgTask").style.opacity = 1;
+	document.getElementById("imgTask").style.zIndex= 1;
+	
+	document.getElementById("imgTaskOptions").style.display = "none";
+	document.getElementById("imgTaskNumber").style.display = "inline";
+	document.getElementById("imgTaskText").style.display = "none";
+	
+	document.getElementById("taskTextImg").innerHTML = text;
+	
+	document.getElementById("imgTaskImg").src = url;
+}
+
+eel.expose(loadImgTaskNumber);
+function loadImgTaskNumber(text, url){
+	document.getElementById("textTask").style.opacity = 0;
+	document.getElementById("textTask").style.zIndex= -1;
+	document.getElementById("imgTask").style.opacity = 1;
+	document.getElementById("imgTask").style.zIndex= 1;
+	
+	document.getElementById("imgTaskOptions").style.display = "none";
+	document.getElementById("imgTaskNumber").style.display = "none";
+	document.getElementById("imgTaskText").style.display = "inline";
+	
+	document.getElementById("taskTextImg").innerHTML = text;
+	
+	document.getElementById("imgTaskImg").src = url;
+}
+
+
 
 eel.expose(unload);
 function unload(){
@@ -29,7 +111,16 @@ function debugShowTaskFormat1(){
 	document.getElementById("imgTask").style.opacity = 0;
 }
 
+function debugShowImgFormatOptions(){
+	document.getElementById("textTask").style.opacity = 0;
+	document.getElementById("textTask").style.zIndex = -1;
+	document.getElementById("imgTask").style.opacity = 1;
+	document.getElementById("imgTask").style.zIndex= 1;
+}
+
 function debugHideTaskFormat1(){
 	document.getElementById("textTask").style.opacity = 0;
 	document.getElementById("imgTask").style.opacity = 0;
+	document.getElementById("textTask").style.zIndex= -1;
+	document.getElementById("imgTask").style.zIndex= -1;
 }
